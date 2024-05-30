1bd1829ddc3673c004fd227909fb1d793d90d3d7
Gennady Gurov <ggurov@gmail.com>
2024-05-30 13:01:16 -0400

    more stuff

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 apt-get -y update
-apt-get -y install make automake autoconf gcc-12-arm-linux-gnueabi gcc-12-arm-linux-gnueabi-base gcc-arm-none-eabi binutils-arm-none-eabi git nano openjdk-21-jdk-headless
+apt-get -y install make automake autoconf gcc-12-arm-linux-gnueabi gcc-12-arm-linux-gnueabi-base gcc-arm-none-eabi binutils-arm-none-eabi git nano openjdk-21-jdk-headless mtools dosfstools
 
