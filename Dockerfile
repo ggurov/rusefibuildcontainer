cd3d6ddb454c5fa9d9201479046f872256a40b25
Gennady Gurov <ggurov@gmail.com>
2025-01-21 22:57:49 -0500

    new stuff

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@
 
 FROM ubuntu:noble
 COPY config.sh /config.sh
+COPY provide_gcc.sh /provide_gcc.sh
+
 RUN /config.sh
+RUN  ln -s /tmp/rusefi-provide_gcc12/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/* /usr/bin/
 CMD /bin/bash
--- config.sh
+++ config.sh
@@ -1,4 +1,14 @@
 
 apt-get -y update
-apt-get -y install make automake autoconf gcc-12-arm-linux-gnueabi gcc-12-arm-linux-gnueabi-base gcc-arm-none-eabi binutils-arm-none-eabi git nano openjdk-21-jdk-headless mtools dosfstools xxd wget gcc-mingw-w64 g++-mingw-w64 zip
+
+apt-get -y install sudo git curl xz-utils openjdk-21-jdk-headless
+apt-get -y install make
+apt-get -y install gcc-multilib g++-multilib g++-mingw-w64 gcc-mingw-w64 sshpass mtools zip dosfstools
+apt-get -y install build-essential gcc gdb gcc-multilib make openjdk-11-jdk-headless xxd 
+apt-get -y install libncurses5 libncursesw5
+
+/./provide_gcc.sh
+
+# apt-get -y install make automake autoconf gcc-12-arm-linux-gnueabi gcc-12-arm-linux-gnueabi-base gcc-arm-none-eabi binutils-arm-none-eabi git nano openjdk-21-jdk-headless mtools dosfstools xxd wget gcc-mingw-w64 g++-mingw-w64 zip gcc g++ 
+# apt-get -y install gcc-multilib g++-multilib
 
