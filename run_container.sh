58e322d15ff659e4f507ef03b22d2e6c6a002c11
Gennady Gurov <ggurov@gmail.com>
2025-02-26 06:33:45 -0500

    pass through simulator ports

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
-docker run --rm -it -v /home/ggurov/Documents/gitrepo/RUS:/rusefi --name rusefi_build rusefi_build:0.1
+docker run --rm -it -v /home/ggurov/Documents/gitrepo/RUS:/rusefi  -p 29001:29001 -p 29002:29002 --name rusefi_build rusefi_build:0.1 
 
 # -v /mnt/c/Users/ggurov/Documents/gitrepo/:/rusefi2 
