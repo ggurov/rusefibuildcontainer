a785cc9cbcf69cc0439bec6d18d40a8939cf0628
Gennady Gurov (stor2) <ggurov@gmail.com>
2025-05-08 05:57:18 -0400

    this one is better

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,1 @@
-docker run --rm -it -v /home/ggurov/Documents/gitrepo/RUS:/rusefi  -p 30001:29001 -p 30002:29002 --name rusefi_build rusefi_build:0.1  $*
-
-# -v /mnt/c/Users/ggurov/Documents/gitrepo/:/rusefi2 
+docker run --rm -it -v `pwd`/repo:/rusefi -p 30001:29001 -p 30002:29002 --name rusefi_build rusefi_build:0.1 $*
