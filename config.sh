dcd8bfb3f7a05d45c8f1f4a7e6fcd6d4d3a214ee
Gennady Gurov <ggurov@gmail.com>
2026-09-09 06:13:53 -0400

    add gh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ apt-get -y install build-essential gcc gdb gcc-multilib make openjdk-11-jdk-head
 apt-get -y install libncurses5 libncursesw5 libarchive-zip-perl bsdextrautils
 apt-get -y install  python3-pip python3 python3-pandas openjfx libopenjfx-java
 apt-get -y install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
-apt-get -y install php npm node.js valgrind bc
+apt-get -y install php npm node.js valgrind bc gh
 apt-get -y install mingw-w64-common mingw-w64-tools mingw-w64 mingw-w64-x86-64-dev mingw-w64-i686-dev g++-mingw-w64
 apt-get -y install g++-mingw-w64-x86-64  gcc-mingw-w64-x86-64  gcc-mingw-w64-base cmake
 apt-get -y install g++-arm-linux-gnueabihf qemu-user
